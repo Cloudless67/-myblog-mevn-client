948b0710073c90380beac97ce122ede0988bd459
Cloudless67 <cloudlesslakitu67@gmail.com>
2021-06-16 16:32:46 +0900

    Removed vue-meta

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { createMetaManager } from 'vue-meta';
 import router from './router';
 import store from './store';
 
 createApp(App)
-    .use(createMetaManager())
     .use(router)
     .use(store)
     .mount('#app');
