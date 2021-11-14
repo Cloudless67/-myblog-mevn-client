b3f95fdcfb500a9091243437d8f24ca2ae648f35
Cloudless67 <cloudlesslakitu67@gmail.com>
2021-11-14 22:22:41 +0900

    [#14] Created theme toggle button

---
+++ src/components/ThemeToggleButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="theme-toggle-container"><input type="checkbox" :checked="modelValue" /></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'ThemeToggleButton',
+    props: {
+        modelValue: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    emits: ['update:modelValue'],
+});
+</script>
