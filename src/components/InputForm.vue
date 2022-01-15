8df025f8c01c499c38e31ed1cd6a431d9e26cd58
Cloudless67 <cloudlesslakitu67@gmail.com>
2022-01-15 21:34:39 +0900

    [#44] Refactor inputs to serparate component

---
+++ src/components/InputForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="mb-2">
+        <label :for="name" class="form-label">{{ label }}</label>
+        <input
+            :id="name"
+            :value="modelValue"
+            class="form-control"
+            :type="type"
+            :disabled="disabled"
+            @input="$emit('update:modelValue', $event.target.value)"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'InputForm',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: 'text',
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        modelValue: {
+            type: String,
+            required: true,
+        },
+    },
+    emits: ['update:modelValue'],
+});
+</script>
